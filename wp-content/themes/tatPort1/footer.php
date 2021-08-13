ea13c297b3f496311f013634dfdba3c67a05acef
evilmopey <44990945+evilmopey@users.noreply.github.com>
2021-08-13 04:32:59 -0500

    Add files via upload

---
+++ wp-content/themes/tatPort1/footer.php
@@ -0,0 +1,7 @@
+        <footer class=" fixed-bottom bg bg-transparent">
+            <p>Executed by NOC</p>
+            <p>Copyright &copy <span id="year"></span>  RONIN ART all rights reserved</p>
+        </footer>
+<?php wp_footer(); ?>
+    </body>
+</html>
