1d3d9de17f4c9fe90613b49f1d8a956e33dd459a
evilmopey <44990945+evilmopey@users.noreply.github.com>
2021-08-13 04:31:43 -0500

    Delete wp-content/themes directory

--- wp-content/themes/tatPort1/footer.php
+++
@@ -1,7 +0,0 @@
-        <footer class=" fixed-bottom bg bg-transparent">
-            <p>Executed by NOC</p>
-            <p>Copyright &copy <span id="year"></span>  RONIN ART all rights reserved</p>
-        </footer>
-<?php wp_footer(); ?>
-    </body>
-</html>
